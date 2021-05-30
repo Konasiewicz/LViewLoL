dd8a8b97d779d870f4489d665bb175a6a897750f
Konasiewicz <84810307+Konasiewicz@users.noreply.github.com>
2021-05-30 05:20:08 +0200

    Update Offsets.h
    11.11.377.6311 offsets UPDATE PATCH 11.11

--- LView/Offsets.h
+++ LView/Offsets.h
@@ -1,114 +1,66 @@
-#pragma once
-#include "ConfigSet.h"
+#include "Offsets.h"
+ 
+Offsets::Offsets() {};
+ 
+int Offsets::GameTime = 0x2FA0294; //	
+ 
+int Offsets::ObjIndex = 0x20; //	
+int Offsets::ObjTeam = 0x4C; //		
+int Offsets::ObjNetworkID = 0xCC; //
+int Offsets::ObjPos = 0x1D8; //		
+int Offsets::ObjVisibility = 0x270; //	
+int Offsets::ObjSpawnCount = 0x284; //	
+int Offsets::ObjSrcIndex = 0x290; //	
+int Offsets::ObjMana = 0x298; //	
+int Offsets::ObjHealth = 0xD98; //	
+int Offsets::ObjMaxHealth = 0xDA8; //	
+int Offsets::ObjArmor = 0x12C8; //	
+int Offsets::ObjMagicRes = 0x12D0; //	
+int Offsets::ObjBaseAtk = 0x12A0; //	
+int Offsets::ObjBonusAtk = 0x1218; //	
+int Offsets::ObjMoveSpeed = 0x12E0; //	
+int Offsets::ObjSpellBook = 0x2408; //	
+int Offsets::ObjName = 0x6C; //		
+int Offsets::ObjLvl = 0x33E0; //		
+int Offsets::ObjExpiry = 0x298; //		
+int Offsets::ObjCrit = 0x12C4; //		
+int Offsets::ObjCritMulti = 0x12B4; //		
+int Offsets::ObjAbilityPower = 0x1228; //	
+int Offsets::ObjAtkSpeedMulti = 0x129C; //	
+int Offsets::ObjItemList = 0x3430; //		
+ 
+int Offsets::ItemListItem = 0xC; //			
+int Offsets::ItemInfo = 0x20; //			
+int Offsets::ItemInfoId = 0x68; //			
+ 
+int Offsets::ViewProjMatrices = 0x2FCBAA0; //		
+int Offsets::Renderer = 0x2FCE914; //		
+int Offsets::RendererWidth = 0x0C; //			
+int Offsets::RendererHeight = 0x10; //			
 
-/// Defines offsets for reading structs from league of legends memory
-class Offsets {
-	
-public:
-	Offsets();
+int Offsets::SpellSlotLevel = 0x20; //			
+int Offsets::SpellSlotTime = 0x28; //			
+int Offsets::SpellSlotDamage = 0x94; //			
+int Offsets::SpellSlotSpellInfo = 0x13C; //			
+int Offsets::SpellInfoSpellData = 0x44; //			
+int Offsets::SpellDataSpellName = 0x6C; //			
+int Offsets::SpellDataMissileName = 0x6C; //			
 
-	static int GameTime;
-	static int GameVersion;
-
-	static int ViewProjMatrices;
-	static int Renderer;
-	static int RendererWidth;
-	static int RendererHeight;
-
-	static int ObjectManager;
-	static int LocalPlayer;
-	static int UnderMouseObject;
-
-	static int Chat;
-	static int ChatIsOpen;
-	static int FnCharacterDataStackUpdate;
-
-	static int ObjIndex;
-	static int ObjTeam;
-	static int ObjNetworkID;
-	static int ObjPos;
-	static int ObjVisibility;
-	static int ObjSpawnCount;
-	static int ObjHealth;
-	static int ObjMaxHealth;
-	static int ObjAbilityHaste;
-	static int ObjLethality;
-	static int ObjMana;
-	static int ObjInvulnerable;
-	static int ObjTargetable;
-	static int ObjRecallState;
-	static int ObjArmor;
-	static int ObjBonusArmor;
-	static int ObjMagicRes;
-	static int ObjBonusMagicRes;
-	static int ObjBaseAtk;
-	static int ObjBonusAtk;
-	static int ObjMoveSpeed;
-	static int ObjSpellBook;
-	static int ObjName;
-	static int ObjLvl;
-	static int ObjTransformation;
-	static int ObjMissileSpellCast;
-	static int ObjExpiry;
-	static int ObjCrit;
-	static int ObjCritMulti;
-	static int ObjAbilityPower;
-	static int ObjAtkSpeedMulti;
-	static int ObjAtkRange;
-	static int ObjMagicPen;
-	static int ObjMagicPenMulti;
-	static int ObjAdditionalApMulti;
-	static int ObjItemList;
-	static int ObjSrcIndex;
-	static int ObjExperience;
-	static int ObjDirection;
-
-	static int ItemListItem;
-	static int ItemInfo;
-	static int ItemInfoId;
-	static int ItemActiveName;
-	static int ItemCharges;
-
-	static int SpellSlotLevel;
-	static int SpellSlotTime;
-	static int SpellSlotDamage;
-	static int SpellSlotSpellInfo;
-	static int SpellInfoSpellData;
-	static int SpellDataSpellName;
-	static int SpellDataMissileName;
-	static int SpellSlotSmiteTimer;
-	static int SpellSlotSmiteCharges;
-	static int SpellSlotCharges;
-	static int SpellSlotTimeCharge;
-	static int SpellSlotValue;
-	static int SpellDataManaArray;
-
-	static int ObjectMapCount;
-	static int ObjectMapRoot;
-	static int ObjectMapNodeNetId;
-	static int ObjectMapNodeObject;
-
-	static int MissileSpellInfo;
-	static int MissileSrcIdx;
-	static int MissileDestIdx;
-	static int MissileStartPos;
-	static int MissileEndPos;
-
-	static int MinimapObject;
-	static int MinimapObjectHud;
-	static int MinimapHudPos;
-	static int MinimapHudSize;
-
-	static int CharacterDataStack;
-	static int CharacterDataStackSkinId;
-
-	static int ObjBuffManager;
-	static int BuffManagerEntriesArray;
-	static int BuffEntryBuff;
-	static int BuffEntryBuffStartTime;
-	static int BuffEntryBuffEndTime;
-	static int BuffName;
-	static int BuffEntryBuffCount;
-	static int BuffEntryBuffNodeStart;
-	static int BuffEntryBuffNodeCurrent;
-};
+int Offsets::ObjectManager = 0x170BBB8; //		
+int Offsets::LocalPlayer = 0x2FA7FE0; //		
+int Offsets::UnderMouseObject = 0x2FA89F8; //		
+int Offsets::ObjectMapCount = 0x2C; //			
+int Offsets::ObjectMapRoot = 0x28; //			
+int Offsets::ObjectMapNodeNetId = 0x10; //			
+int Offsets::ObjectMapNodeObject = 0x14; //			
+ 
+int Offsets::MissileSpellInfo = 0x258; //			
+int Offsets::MissileSrcIdx = 0x2B8; //			
+int Offsets::MissileDestIdx = 0x310; //			
+int Offsets::MissileStartPos = 0x2D0; //			
+int Offsets::MissileEndPos = 0x2DC; //			
+int Offsets::MinimapObject = 0x2FA7FDC; //		
+ 
+int Offsets::MinimapObjectHud = 0x88; //			
+int Offsets::MinimapHudPos = 0x60; //			
+int Offsets::MinimapHudSize = 0x68; //			
